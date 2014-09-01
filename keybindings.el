ab99204f7a442b7fa54d4be9926bda17b98d81e8
Ken Stevens <kensubuntu@gmail.com>
2014-09-01 14:20:26 -0400

    Added a couple of keybindings

--- keybindings.el
+++ keybindings.el
@@ -5,6 +5,9 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-g" 'gtd)
-(global-set-key "\C-c!)" 'org-time-stamp-inactive)
+(global-set-key "\C-c!" 'org-time-stamp-inactive)
+(global-set-key "\C-cm" 'magit-status)
+(global-set-key "\C-ciud" 'insert-user-timestamp)
+(global-set-key "\C-cid" 'insert-date)
 
 
